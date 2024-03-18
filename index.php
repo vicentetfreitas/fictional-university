6db1d422b1ea91e7d85ce871d7d5430d8f1e9245
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-03-17 22:07:00 -0300

    O famoso “Loop” no WordPress

--- index.php
+++ index.php
@@ -1,28 +1,14 @@
 <?php
-function greet($name, $color)
-{
-    echo  "<p>Hi, my name is $name and my favorite $color is blank.</p>";
-}
 
-greet('Vicente',  'red');
-greet('Freitas',  'blue');
-
-$names = array('Vicente', 'Teixeira', 'de', 'Freitas');
-$count = 1;
-
-while($count <= 10){
-    echo"<li>$count</li>";
-    $count++;
-}
-
-while($count <= count($names)){
-    echo"<li>Hi, my name is $names[$count]</li>";
-    $count++;
-}
+while (have_posts()) {
+    the_post(); ?>
+    <h2>
+        <a href="<?php the_permalink(); ?>">
+            <?php the_title(); ?>
+        </a>
+    </h2>
+    <p><?php the_content(); ?></p>
+    <hr>
+<?php }
 
 ?>
-
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
-
-<p>Hi, my name is <?php echo $names[0]; ?></p>
