1dc84614bfb0ec71d7b923f42279f35b3544af2a
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-03-19 17:21:06 -0300

    Menu de navegação (Continuação)

--- footer.php
+++ footer.php
@@ -12,22 +12,23 @@
         <div class="site-footer__col-two">
           <h3 class="headline headline--small">Explore</h3>
           <nav class="nav-list">
-            <?php
-            wp_nav_menu(array(
-              'theme_location' => 'footerLocationOne'
-            ));
-            ?>
+            <ul>
+              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+              <li><a href="<?php echo site_url('/') ?>">Programs</a></li>
+              <li><a href="<?php echo site_url('/') ?>">Events</a></li>
+              <li><a href="<?php echo site_url('/') ?>">Campuses</a></li>
+            </ul>
           </nav>
         </div>
 
         <div class="site-footer__col-three">
           <h3 class="headline headline--small">Learn</h3>
           <nav class="nav-list">
-            <?php
-            wp_nav_menu(array(
-              'theme_location' => 'footerLocationTwo'
-            ));
-            ?>
+            <ul>
+              <li><a href="<?php echo site_url('/') ?>">Legal</a></li>
+              <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
+              <li><a href="<?php echo site_url('/') ?>">Careers</a></li>
+            </ul>
           </nav>
         </div>
       </div>
--- header.php
+++ header.php
@@ -17,11 +17,15 @@
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
-          <?php
-          wp_nav_menu(array(
-            'theme_location' => 'headerMenuLocation'
-          ));
-          ?>
+          <ul>
+            <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 15 or wp_get_post_parent_id(0) == 17) echo 'class="current-menu-item"' ?>>
+              <a href="<?php echo site_url('/about-us') ?>">About Us</a>
+            </li>
+            <li><a href="<?php echo site_url('/') ?>">Programs</a></li>
+            <li><a href="<?php echo site_url('/') ?>">Events</a></li>
+            <li><a href="<?php echo site_url('/') ?>">Campuses</a></li>
+            <li><a href="<?php echo site_url('/') ?>">Blog</a></li>
+          </ul>
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
