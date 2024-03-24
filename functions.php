d99574844c28dc711356dd4a81a15aaa30475584
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-03-24 17:44:51 -0300

    Tempo limite rápido: atualizações diversas

--- functions.php
+++ functions.php
@@ -24,12 +24,13 @@ add_action('after_setup_theme', 'university_features');
 function university_post_types()
 {
     register_post_type('event', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array(
             'slug' => 'events'
         ),
         'has_archive' => true,
         'public' => true,
-        'show_in_rest' => true,
         'labels' => array(
             'name' => 'Events',
             'add_new_item' => 'Add New Event',
