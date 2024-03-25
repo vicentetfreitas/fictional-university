6bfd8a821157217dbbdbe8df891282eedd7bbe12
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-03-25 11:29:10 -0300

    Manipulando consultas baseadas em URL padrão

--- functions.php
+++ functions.php
@@ -19,8 +19,26 @@ function university_features()
 }
 add_action('after_setup_theme', 'university_features');
 
-// Plugins
+function university_adjust_queries($query)
+{
+    if (!is_admin() and  is_post_type_archive('event') and $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        ));
+    }
+}
+add_action('pre_get_posts', 'university_adjust_queries');
 
+// Plugins
 function university_post_types()
 {
     register_post_type('event', array(
@@ -43,5 +61,4 @@ function university_post_types()
 }
 
 add_action('init', 'university_post_types');
-
 // Plugins end
