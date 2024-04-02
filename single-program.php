3066a890de995132ebff4c281287ad2063442f29
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-02 08:28:15 -0300

    Tamanhos e cortes de imagens em destaque

--- single-program.php
+++ single-program.php
@@ -49,7 +49,7 @@ while (have_posts()) {
                 $relatedProfessors->the_post(); ?>
                 <li class="professor-card__list-item">
                     <a class="professor-card" href="<?php the_permalink(); ?>">
-                        <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>" alt="">
+                        <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>" alt="">
                         <span class="professor-card__name">
                             <?php the_title(); ?>
                         </span>
