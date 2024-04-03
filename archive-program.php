0e627c0aef6494fd3440cdd420f8f928a18ff278
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-03 18:55:15 -0300

    Usando nossa função "pageBanner"

--- archive-program.php
+++ archive-program.php
@@ -1,16 +1,13 @@
-<?php get_header(); ?>
+<?php
 
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            All Programs
-        </h1>
-        <div class="page-banner__intro">
-            <p>There is something for everyone. Have look around.</p>
-        </div>
-    </div>
-</div>
+get_header();
+pageBanner(array(
+    'title'    => 'All Programs',
+    'subtitle' => 'There is something for everyone. Have look around.'
+
+));
+
+?>
 
 <div class="container container--narrow page-section">
     <ul class="link-list min-list">
--- single-professor.php
+++ single-professor.php
@@ -1,4 +1,6 @@
-<?php get_header();
+<?php 
+
+get_header();
 while (have_posts()) {
     the_post();
     pageBanner();
