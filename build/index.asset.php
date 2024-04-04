dfeabf8bec1f8b8346b960df3a7c222f59f8235d
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-04 09:30:22 -0300

    Mapa do campus no front-end

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'a017292e8c2efc488ec7');
+<?php return array('dependencies' => array(), 'version' => '87ab74121c5292027032');
--- functions.php
+++ functions.php
@@ -30,6 +30,7 @@ function pageBanner($args = NULL)
 
 function university_files()
 {
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyCeK-YsjVnchsFzjn7FQGIVmBbjsr0rtKc', NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', ('//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i'));
     wp_enqueue_style('font-awesone', ('//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'));
@@ -79,7 +80,7 @@ add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api)
 {
-    $api['key'] = 'AIzaSyBWe5yTiUuXx8mlGDVRmHlKNNU9WM30zkE';
+    $api['key'] = 'AIzaSyCeK-YsjVnchsFzjn7FQGIVmBbjsr0rtKc';
     return $api;
 }
 add_filter('acf/fields/google_map/api', 'universityMapKey');
