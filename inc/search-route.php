f2c6e4804ea7612bed8457095972e876d0600f61
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-05 17:15:38 -0300

    API REST: Adicionar nova rota personalizada (URL)

---
+++ inc/search-route.php
@@ -0,0 +1,15 @@
+<?php
+
+function universityRegisterSearch()
+{
+    register_rest_route('university/v1', 'search', array(
+        'methods' => WP_REST_SERVER::READABLE,
+        'callback' => 'universitySearchResults'
+    ));
+}
+add_action('rest_api_init', 'universityRegisterSearch');
+
+function universitySearchResults()
+{
+    return 'Congratulations, you created a route.';
+}
