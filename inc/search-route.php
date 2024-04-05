99e0d6a62e6b05a3aeca53018d8096fc0a368e86
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-05 17:40:55 -0300

    Crie seus próprios dados JSON brutos

--- inc/search-route.php
+++ inc/search-route.php
@@ -11,5 +11,16 @@ add_action('rest_api_init', 'universityRegisterSearch');
 
 function universitySearchResults()
 {
-    return 'Congratulations, you created a route.';
+    $professors = new WP_Query(array(
+        'post_type' => 'professor'
+    ));
+    $professorResults = array();
+    while ($professors->have_posts()) {
+        $professors->the_post();
+        array_push($professorResults, array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+        ));
+    };
+    return $professorResults;
 }
