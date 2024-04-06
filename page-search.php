4581554b60691ba3521b3ffe17464b7fc751f1a5
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-06 12:19:02 -0300

    Pesquisa tradicional em WordPress (Parte 2)

--- page-search.php
+++ page-search.php
@@ -50,13 +50,7 @@ while (have_posts()) {
         <?php } ?>
 
         <div class="generic-content">
-            <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-                <label class="headline headline--medium" for="s">Perform a New Search:</label>
-                <div class="search-form-row">
-                    <input class="s" id="s" type="search" name="s" placeholder="What are you looking for?">
-                    <input class="search-submit" type="submit" value="Search">
-                </div>
-            </form>
+            <?php get_search_form(); ?>
         </div>
     </div>
 
