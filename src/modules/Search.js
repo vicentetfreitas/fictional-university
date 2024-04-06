327e57bac344a183049ec87dd75b2c7821f2624f
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-06 10:49:01 -0300

    Pesquisa tradicional em WordPress

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -4,54 +4,54 @@ class Search {
     // 1. describe and create/initiate our object
     constructor() {
         this.addSearchHTML()
-        this.resultsDiv = document.querySelector("#search-overlay__results")
-        this.openButton = document.querySelectorAll(".js-search-trigger")
-        this.closeButton = document.querySelector(".search-overlay__close")
-        this.searchOverlay = document.querySelector(".search-overlay")
-        this.searchField = document.querySelector("#search-term")
-        this.isOverlayOpen = false
-        this.isSpinnerVisible = false
-        this.previousValue
-        this.typingTimer
-        this.events()
+        this.resultsDiv = document.querySelector("#search-overlay__results");
+        this.openButton = document.querySelectorAll(".js-search-trigger");
+        this.closeButton = document.querySelector(".search-overlay__close");
+        this.searchOverlay = document.querySelector(".search-overlay");
+        this.searchField = document.querySelector("#search-term");
+        this.isOverlayOpen = false;
+        this.isSpinnerVisible = false;
+        this.previousValue;
+        this.typingTimer;
+        this.events();
     }
 
     // 2. events
     events() {
         this.openButton.forEach(el => {
             el.addEventListener("click", e => {
-                e.preventDefault()
-                this.openOverlay()
+                e.preventDefault();
+                this.openOverlay();
             })
         })
 
-        this.closeButton.addEventListener("click", () => this.closeOverlay())
-        document.addEventListener("keydown", e => this.keyPressDispatcher(e))
-        this.searchField.addEventListener("keyup", () => this.typingLogic())
+        this.closeButton.addEventListener("click", () => this.closeOverlay());
+        document.addEventListener("keydown", e => this.keyPressDispatcher(e));
+        this.searchField.addEventListener("keyup", () => this.typingLogic());
     }
 
     // 3. methods (function, action...)
     typingLogic() {
         if (this.searchField.value != this.previousValue) {
-            clearTimeout(this.typingTimer)
+            clearTimeout(this.typingTimer);
             if (this.searchField.value) {
                 if (!this.isSpinnerVisible) {
-                    this.resultsDiv.innerHTML = '<div class="spinner-loader"></div>'
-                    this.isSpinnerVisible = true
+                    this.resultsDiv.innerHTML = '<div class="spinner-loader"></div>';
+                    this.isSpinnerVisible = true;
                 }
-                this.typingTimer = setTimeout(this.getResults.bind(this), 750)
+                this.typingTimer = setTimeout(this.getResults.bind(this), 750);
             } else {
-                this.resultsDiv.innerHTML = ""
-                this.isSpinnerVisible = false
+                this.resultsDiv.innerHTML = "";
+                this.isSpinnerVisible = false;
             }
         }
-        this.previousValue = this.searchField.value
+        this.previousValue = this.searchField.value;
     }
 
     async getResults() {
         try {
-            const response = await axios.get(universityData.root_url + "/wp-json/university/v1/search?term=" + this.searchField.value)
-            const results = response.data
+            const response = await axios.get(universityData.root_url + "/wp-json/university/v1/search?term=" + this.searchField.value);
+            const results = response.data;
             this.resultsDiv.innerHTML = `
         <div class="row">
           <div class="one-third">
@@ -100,36 +100,36 @@ class Search {
               `).join("")}
           </div>
         </div> `
-            this.isSpinnerVisible = false
+            this.isSpinnerVisible = false;
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
     }
 
     keyPressDispatcher(e) {
         if (e.keyCode == 83 && !this.isOverlayOpen && document.activeElement.tagName != "INPUT" && document.activeElement.tagName != "TEXTAREA") {
-            this.openOverlay()
+            this.openOverlay();
         }
         if (e.keyCode == 27 && this.isOverlayOpen) {
-            this.closeOverlay()
+            this.closeOverlay();
         }
     }
 
     openOverlay() {
-        this.searchOverlay.classList.add("search-overlay--active")
-        document.body.classList.add("body-no-scroll")
-        this.searchField.value = ""
-        setTimeout(() => this.searchField.focus(), 301)
-        console.log("our open method just ran!")
-        this.isOverlayOpen = true
-        return false
+        this.searchOverlay.classList.add("search-overlay--active");
+        document.body.classList.add("body-no-scroll");
+        this.searchField.value = "";
+        setTimeout(() => this.searchField.focus(), 301);
+        console.log("our open method just ran!");
+        this.isOverlayOpen = true;
+        return false;
     }
 
     closeOverlay() {
-        this.searchOverlay.classList.remove("search-overlay--active")
-        document.body.classList.remove("body-no-scroll")
-        console.log("our close method just ran!")
-        this.isOverlayOpen = false
+        this.searchOverlay.classList.remove("search-overlay--active");
+        document.body.classList.remove("body-no-scroll");
+        console.log("our close method just ran!");
+        this.isOverlayOpen = false;
     }
 
     addSearchHTML() {
@@ -147,7 +147,7 @@ class Search {
             <div id="search-overlay__results"></div>
             </div>
         </div> `
-        )
+        );
     }
 }
-export default Search
+export default Search;
