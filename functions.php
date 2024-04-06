c47640b62eaa4a9b763795ba22ca6f7e13e3759a
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-06 17:05:23 -0300

    Inscrições abertas

--- functions.php
+++ functions.php
@@ -185,3 +185,28 @@ function university_post_types()
 }
 
 add_action('init', 'university_post_types');
+
+
+// Redirect subscriber accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend()
+{
+    $ourCurrentUser = wp_get_current_user();
+
+    if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+        wp_redirect(site_url('/'));
+        exit;
+    }
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar()
+{
+    $ourCurrentUser = wp_get_current_user();
+
+    if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+        show_admin_bar(false);
+    }
+}
