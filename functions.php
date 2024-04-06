203716e176a48e8aea59cc2ed78b9933e33c47b1
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-05 22:38:44 -0300

    Lógica de pesquisa que conhece os relacionamentos (parte 2)

--- functions.php
+++ functions.php
@@ -148,7 +148,7 @@ function university_post_types()
     // Program Post Type
     register_post_type('program', array(
         'show_in_rest' => true,
-        'supports' => array('title', 'editor'),
+        'supports' => array('title'),
         'rewrite' => array(
             'slug' => 'programs'
         ),
