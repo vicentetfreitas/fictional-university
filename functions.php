7ce7ce327273ebeb60327af3223fc279b32db714
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-04-07 18:13:12 -0300

    Nota Permissões e Segurança (Parte 2)

--- functions.php
+++ functions.php
@@ -253,6 +253,10 @@ add_filter('login_headertitle', 'ourLoginTitle');
 // Force note posts to be private
 function makeNotePrivate($data)
 {
+    if ($data['post_type'] == 'note') {
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+    }
     if ($data['post_type'] == 'note' and $data['post_status'] != 'trash') {
         $data['post_status'] = "private";
     }
