d14946be3aa32da3562d566188c827ed04d0ee1f
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2024-06-09 09:56:28 -0300

    What is theme.json in a block theme

--- wp-content/themes/fictional-block-theme/build/banner.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => '611fad48ffa5cf56f148');
--- wp-content/themes/fictional-block-theme/build/genericheading.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '92bbf238d98296281d10');
